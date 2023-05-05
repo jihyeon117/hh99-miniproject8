8067cf3dfe07fef0af96d4e94b022b1354df2ac6
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2023-05-05 15:34:08 +0900

    Add files via upload

---
+++ src/main/java/com/example/hh99miniproject8/Hh99Miniproject8Application.java
@@ -0,0 +1,13 @@
+package com.example.hh99miniproject8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99Miniproject8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99Miniproject8Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hh99miniproject8/Hh99Miniproject8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hh99miniproject8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh99Miniproject8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
