7bc3dac8430645914f8395fbea2f40265757366a
SeokBin02 <ghkdtjrqls02@gmail.com>
2023-05-06 00:37:33 +0900

    [Feat] 로그인 기능 구현

--- src/main/java/com/example/hh99miniproject8/service/UserService.java
+++ src/main/java/com/example/hh99miniproject8/service/UserService.java
@@ -1,5 +1,6 @@
 package com.example.hh99miniproject8.service;
 
+import com.example.hh99miniproject8.dto.user.LoginReqeustDto;
 import com.example.hh99miniproject8.dto.user.SignupReqeustDto;
 import com.example.hh99miniproject8.dto.user.SignupResponseDto;
 import com.example.hh99miniproject8.entity.RoleTypeEnum;
@@ -32,5 +33,17 @@ public class UserService {
         // 입력된 회원 정보 저장
         userRepository.save(new User(reqeust, role));
         return new SignupResponseDto("회원가입에 성공하셨습니다.");
+    }
+
+    public void login(LoginReqeustDto reqeust) {
+        // 입력한 ID를 기반으로 회원 존재 유무 체크
+        User user = userRepository.findByUsername(reqeust.getUsername()).orElseThrow(
+                () -> new NullPointerException("회원이 존재하지 않습니다.")
+        );
+
+        // 비밀번호 일치여부 체크
+        if(!user.getPassword().equals(reqeust.getPassword())){
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
     }
 }
