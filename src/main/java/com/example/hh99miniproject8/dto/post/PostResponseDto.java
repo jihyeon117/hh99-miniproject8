2e1d9e95a052e68e9e73a568888c919e7766b3d3
JEON <jwodn123@naver.com>
2023-05-05 23:16:55 +0900

    feat : 게시글 CRUD API 생성

---
+++ src/main/java/com/example/hh99miniproject8/dto/post/PostResponseDto.java
@@ -0,0 +1,36 @@
+package com.example.hh99miniproject8.dto.post;
+
+import com.example.hh99miniproject8.entity.Post;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@NoArgsConstructor
+public class PostResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createdAt;
+    //private LocalDateTime modifiedAt;
+    private String category;
+    private int goodCount;
+    //private List<CommentResponseDTO> comments;
+
+    public PostResponseDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.content = post.getContent();
+        this.createdAt = post.getCreatedAt();
+        //this.modifiedAt = post.getModifiedAt();
+        this.category = post.getCategory();
+        this.goodCount = post.getGoodCount();
+        //this.comments = post.getComments().stream().map(CommentResponseDto::new).collect(Collectors.toList());
+    }
+
+
+}
