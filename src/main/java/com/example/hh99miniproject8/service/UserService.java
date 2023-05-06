25fcb5a55a5bd1183910dc26a603a3e673922943
Seokbin02 <ghkdtjrqls02@gmail.com>
2023-05-06 10:57:39 +0900

    [Feat] jwtUtil 추가, 로그인시 토큰 반환

--- src/main/java/com/example/hh99miniproject8/service/UserService.java
+++ src/main/java/com/example/hh99miniproject8/service/UserService.java
@@ -2,46 +2,55 @@ package com.example.hh99miniproject8.service;
 
 import com.example.hh99miniproject8.dto.user.LoginRequestDto;
 import com.example.hh99miniproject8.dto.user.SignupRequestDto;
-import com.example.hh99miniproject8.dto.user.SignupResponseDto;
 import com.example.hh99miniproject8.entity.RoleTypeEnum;
 import com.example.hh99miniproject8.entity.User;
+import com.example.hh99miniproject8.exception.CustomException;
+import com.example.hh99miniproject8.exception.ErrorCode;
 import com.example.hh99miniproject8.repository.UserRepository;
+import com.example.hh99miniproject8.security.jwt.JwtUtil;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
     private static final String ADMIN_TOKEN = "AAABnvxRVklrnYxKZ0aHgTBcXukeZygoC";
-    public SignupResponseDto signup(SignupRequestDto reqeust) {
+    public ResponseEntity<String> signup(SignupRequestDto reqeust) {
         // 아이디 중복 체크
         if(userRepository.findByUsername(reqeust.getUsername()).isPresent())
-            throw new IllegalArgumentException("중복된 아이디가 존재합니다.");
+            throw new CustomException(ErrorCode.USER_Duplicate);
 
         RoleTypeEnum role = RoleTypeEnum.USER;
         // 관리자 인가 체크
         if(reqeust.isAdmin()){
-            if(reqeust.getAuthKey() == null)
-                throw new NullPointerException("관리자 인증키를 입력하지 않았습니다.");
+            if(reqeust.getAuthKey() == null || !reqeust.getAuthKey().equals(ADMIN_TOKEN))
+                throw new CustomException(ErrorCode.AUTHKEY_NOT_FOUND);
             if(reqeust.getAuthKey().equals(ADMIN_TOKEN))
                 role = RoleTypeEnum.ADMIN;
         }
 
         // 입력된 회원 정보 저장
         userRepository.save(new User(reqeust, role));
-        return new SignupResponseDto("회원가입에 성공하셨습니다.");
+        return ResponseEntity.status(HttpStatus.OK).body("회원가입 성공!");
     }
 
-    public void login(LoginRequestDto request) {
+    public ResponseEntity<String> login(LoginRequestDto request, HttpServletResponse response) {
         // 입력한 ID를 기반으로 회원 존재 유무 체크
         User user = userRepository.findByUsername(request.getUsername()).orElseThrow(
-                () -> new NullPointerException("회원이 존재하지 않습니다.")
+                () -> new CustomException(ErrorCode.USER_NOT_FOUND)
         );
 
         // 비밀번호 일치여부 체크
         if(!user.getPassword().equals(request.getPassword())){
-            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+            throw new CustomException(ErrorCode.INVALIED_PASSWORD);
         }
+
+        response.addHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(user.getUsername(), user.getRole()));
+        return ResponseEntity.status(HttpStatus.OK).body("로그인 성공!");
     }
 }
