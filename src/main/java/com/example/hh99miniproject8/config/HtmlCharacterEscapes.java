31d588bcef2d8299216468247746742f2ab33d2d
LEOJI <122586785+jihyeon117@users.noreply.github.com>
2023-05-09 17:46:28 +0900

    서버 때문에 잠시 주석처리합니다.

--- src/main/java/com/example/hh99miniproject8/config/HtmlCharacterEscapes.java
+++ src/main/java/com/example/hh99miniproject8/config/HtmlCharacterEscapes.java
@@ -1,33 +1,33 @@
-package com.example.hh99miniproject8.config;
+// package com.example.hh99miniproject8.config;
 
-import com.fasterxml.jackson.core.SerializableString;
-import com.fasterxml.jackson.core.io.CharacterEscapes;
-import com.fasterxml.jackson.core.io.SerializedString;
-import org.apache.commons.text.StringEscapeUtils;
+// import com.fasterxml.jackson.core.SerializableString;
+// import com.fasterxml.jackson.core.io.CharacterEscapes;
+// import com.fasterxml.jackson.core.io.SerializedString;
+// import org.apache.commons.text.StringEscapeUtils;
 
-public class HtmlCharacterEscapes extends CharacterEscapes {
+// public class HtmlCharacterEscapes extends CharacterEscapes {
 
-    private final int[] asciiEscapes;
+//     private final int[] asciiEscapes;
 
-    public HtmlCharacterEscapes() {
-        // XSS 방지 처리할 특수 문자 지정
-        asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
-        asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
-        asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
-    }
+//     public HtmlCharacterEscapes() {
+//         // XSS 방지 처리할 특수 문자 지정
+//         asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
+//         asciiEscapes['<'] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes['>'] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes['\"'] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes['('] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes[')'] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes['#'] = CharacterEscapes.ESCAPE_CUSTOM;
+//         asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
+//     }
 
-    @Override
-    public int[] getEscapeCodesForAscii() {
-        return asciiEscapes;
-    }
+//     @Override
+//     public int[] getEscapeCodesForAscii() {
+//         return asciiEscapes;
+//     }
 
-    @Override
-    public SerializableString getEscapeSequence(int ch) {
-        return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
-    }
-}
+//     @Override
+//     public SerializableString getEscapeSequence(int ch) {
+//         return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+//     }
+// }
