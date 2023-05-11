d79d8b41c05dbe25709927122c7918cb3f03c3d3
JEON <jwodn123@naver.com>
2023-05-11 10:16:31 +0900

    fix : xss 방지 필터에 대한 설명 주석 추가

--- src/main/java/com/example/hh99miniproject8/config/HtmlCharacterEscapes.java
+++ src/main/java/com/example/hh99miniproject8/config/HtmlCharacterEscapes.java
@@ -5,10 +5,13 @@ import com.fasterxml.jackson.core.io.CharacterEscapes;
 import com.fasterxml.jackson.core.io.SerializedString;
 import org.apache.commons.text.StringEscapeUtils;
 
+// JSON 응답의 특수 문자를 이스케이프하여 청XSS 공격을 방지
+// CharacterEscapes : JSON 문자열에서 문자를 이스케이프하는 방법을 정의하는 Jackson 클래스
 public class HtmlCharacterEscapes extends CharacterEscapes {
 
     private final int[] asciiEscapes;
 
+    // 사용자 지정 이스케이프 시퀀스로 대체해야 하는 ASCII 이스케이프 코드 배열을 설정
     public HtmlCharacterEscapes() {
         // XSS 방지 처리할 특수 문자 지정
         asciiEscapes = CharacterEscapes.standardAsciiEscapesForJSON();
@@ -21,11 +24,14 @@ public class HtmlCharacterEscapes extends CharacterEscapes {
         asciiEscapes['\''] = CharacterEscapes.ESCAPE_CUSTOM;
     }
 
+    // 생성자에 설정된 ASCII 이스케이프 코드 배열을 반환합니다.
     @Override
     public int[] getEscapeCodesForAscii() {
         return asciiEscapes;
     }
 
+    // 지정된 문자의 사용자 지정 이스케이프 시퀀스를 나타내는 SerializableString 개체를 반환
+    // Apache Commons Lang 라이브러리의 StringEscapeUtils.escapeHtml4() 메서드를 사용하여 문자를 HTML로 이스케이프 한다.
     @Override
     public SerializableString getEscapeSequence(int ch) {
         return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
--- src/main/java/com/example/hh99miniproject8/config/WebMvcConfig.java
+++ src/main/java/com/example/hh99miniproject8/config/WebMvcConfig.java
@@ -10,10 +10,13 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 @Slf4j
 @RequiredArgsConstructor
 @Configuration
-public class WebMvcConfig { //JSON API에 XSS Filter 적용
+// JSON 직렬화에 사용자 지정 HtmlCharacterEscapes 클래스를 사용하도록 ObjectMapper 인스턴스를 설정하는 클래스.
+// Spring MVC 프레임워크에서 JSON 응답을 변환하는 데 사용
+public class WebMvcConfig {
 
     private final ObjectMapper objectMapper;
 
+    // MappingJackson2HttpMessageConverter의 인스턴스를 반환
     @Bean
     public MappingJackson2HttpMessageConverter jsonEscapeConverter() {
         ObjectMapper copy = objectMapper.copy();
